0a56d3a6f1cf0fb1c945353fffaefa57f4cccc80
AndreyKhodulapov <andrey.corsar1993@gmail.com>
2025-08-24 18:10:39 +0300

    feat: make eof

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from controller import main
 logger = getLogger(__name__)
 
 if __name__ == "__main__":
-    logger.info("start app")
-    main()
-    logger.info("stop app")
+    try:
+        logger.info("start app")
+        main()
+    except (KeyboardInterrupt, EOFError):
+        print("END")
+        logger.info("stop app")
